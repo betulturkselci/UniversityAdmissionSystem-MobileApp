76be33244f36c52f03cfe78008994f33edcd5b0c
betulturkselci <you@example.com>
2021-11-09 12:29:34 +0300

    first commit

---
+++ university_app/lib/main.dart
@@ -0,0 +1,23 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:university_app/screens/home.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cupertino_icons/cupertino_icons.dart';
+import 'package:university_app/models/createpdf.dart';
+
+
+Future <void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),      
+      );
+  }
+}
